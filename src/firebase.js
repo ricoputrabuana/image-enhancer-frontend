abfb9352ce24d08b59f6d8199e40440e576b2cb7
ricoputrabuana <149350841+ricoputrabuana@users.noreply.github.com>
2025-07-07 05:44:27 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDMW4ppRp18vZPvrAlgTJs1AABaMHvPfes",
-  authDomain: "imageenhancer-d012d.firebaseapp.com",
-  projectId: "imageenhancer-d012d",
-  storageBucket: "imageenhancer-d012d.firebasestorage.app",
-  messagingSenderId: "505328322598",
-  appId: "1:505328322598:web:24a1cca81def091691bee7"
+  apiKey: "process.env.AIzaSyDMW4ppRp18vZPvrAlgTJs1AABaMHvPfes",
+  authDomain: "process.env.imageenhancer-d012d.firebaseapp.com",
+  projectId: "process.env.imageenhancer-d012d",
+  storageBucket: "process.env.imageenhancer-d012d.firebasestorage.app",
+  messagingSenderId: "process.env.505328322598",
+  appId: "process.env.1:505328322598:web:24a1cca81def091691bee7"
 };
 
 const app = initializeApp(firebaseConfig);
