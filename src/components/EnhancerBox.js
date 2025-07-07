05dcccf135298300604b562d65e079d93b48a54d
ricoputrabuana <149350841+ricoputrabuana@users.noreply.github.com>
2025-07-07 08:14:35 +0700

    Update EnhancerBox.js

--- src/components/EnhancerBox.js
+++ src/components/EnhancerBox.js
@@ -36,7 +36,7 @@ function EnhancerBox({ user, userData, onCoinUpdate }) {
     formData.append('image', selectedFile);
 
     try {
-      const res = await axios.post('https://ricoputra1708-image-enhancer.hf.space/upload', formData, {
+      const res = await axios.post('https://ricoputra1708-image-enhancer.hf.space/run/predict', formData, {
         responseType: 'blob',
       });
       const imageURL = URL.createObjectURL(res.data);
