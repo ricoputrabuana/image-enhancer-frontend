5eacb662b3406366b55006053ef8dd6d657bdfcd
ricoputrabuana <149350841+ricoputrabuana@users.noreply.github.com>
2025-07-14 22:22:21 +0700

    Update EnhancerBox.js

--- src/components/EnhancerBox.js
+++ src/components/EnhancerBox.js
@@ -40,7 +40,7 @@ const handleEnhance = async () => {
 
     try {
       const res = await axios.post(
-        'https://image-proxy-server-production.up.railway.app/proxy',
+        'https://image-proxy-server-production.up.railway.app/proxy/',
         {
           data: [base64Image]
         },
