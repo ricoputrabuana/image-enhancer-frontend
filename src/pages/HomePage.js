125499b27b7b8be65e11d6e6222426a1734bc49d
ricoputrabuana <149350841+ricoputrabuana@users.noreply.github.com>
2026-05-25 23:21:19 +0700

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,10 @@
 // src/pages/HomePage.js
 import React from 'react';
-import axios from 'axios';
 import EnhancerBox from '../components/EnhancerBox';
 
 function HomePage({ user, userData, setUserData }) {
-
    return (
     <div style={{ paddingTop:'50px', textAlign:'center' }}>
-
       <EnhancerBox
         user={user}
         userData={userData}
@@ -18,7 +15,6 @@ function HomePage({ user, userData, setUserData }) {
           }));
         }}
       />
-
     </div>
   );
 }
