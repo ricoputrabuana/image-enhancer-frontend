1a9daa419670ccb7c520b33d9cc6f81c024932ff
ricoputrabuana <149350841+ricoputrabuana@users.noreply.github.com>
2026-05-25 13:16:42 +0700

    Update EnhancerBox.js

--- src/components/EnhancerBox.js
+++ src/components/EnhancerBox.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import './EnhancerBox.css';
 import { doc, updateDoc } from 'firebase/firestore';
