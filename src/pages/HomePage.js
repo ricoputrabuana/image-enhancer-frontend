0a0be96ffbc4bd18df7c11256a680ae97a94f4bd
ricoputrabuana <149350841+ricoputrabuana@users.noreply.github.com>
2026-05-25 23:19:11 +0700

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,4 +5,21 @@ import EnhancerBox from '../components/EnhancerBox';
 
 function HomePage({ user, userData, setUserData }) {
 
+   return (
+    <div style={{ paddingTop:'50px', textAlign:'center' }}>
+
+      <EnhancerBox
+        user={user}
+        userData={userData}
+        onCoinUpdate={(newCoins)=>{
+          setUserData(prev=>({
+            ...prev,
+            coins:newCoins
+          }));
+        }}
+      />
+
+    </div>
+  );
+
 export default HomePage;
